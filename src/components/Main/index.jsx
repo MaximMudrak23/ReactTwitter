adcaa39c3572df60e4d9d6f51121a33e89afaf97
Maxim <mudrak.maksym@chnu.edu.ua>
2025-01-15 19:16:33 +0200

    Register Page Design+Media

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,5 +1,5 @@
 import './styles.css'
-import './steps.css'
+import './rStyles.css'
 import { RoundButton } from '../RoundButton'
 import twitterLogo from '/twitter-logo.svg'
 import googleLogo from '/google-logo.svg'
@@ -9,55 +9,68 @@ export function Main() {
     return (
         <>
         <main>
-            <section className='leftSection'>
-                <div className='leftSectionDiv'><img src={twitterLogo} alt="Twitter Logo" draggable='false' /></div>
+            <section className='lSection'>
+                <img src={twitterLogo} alt="Twitter Logo" draggable='false' />
             </section>
-            <section className='rightSection'>
-                <div className='rightSectionDiv'>
-                    <div className='stepOne'>
-                        <p className='part1' >В курсе <br /> происходящего</p>
-                        <p className='part2' >Присоединяйтесь сегодня.</p>
-                    </div>
-                    <div className='stepTwo'>
-                        <div className='part1'>
+            <section className='rSection'>
+                <div className="rSection__pt2">
+                    <div className="rSection__pt3">
+                        <div className="rSection__welcome1">
+                        <p>В курсе <br /> происходящего</p>
+                        </div>
+                        <div className="rSection__welcome2">
+                            <p>Присоединяйтесь сегодня.</p>
+                        </div>
+                        <div className="rSection__googleButton">
                             <RoundButton 
-                            bgc={'#121212'} 
-                            brc={'#343436'} 
-                            txtc={'#C2CBD1'} 
-                            txt={'Регистрация с помощью Google'} 
+                            bgc={'rgba(18, 18, 18, 1)'}
+                            brc={'rgba(54, 54, 54, 1)'}
+                            txtc={'white'}
+                            txt={'Регистрация с помощью Google'}
                             logo={googleLogo}
-                            logoAlt={'Google Logo'} />
+                            logoAlt={'Google Logo'}
+                            />
+                        </div>
+                        <div className="rSection__appleButton">
+                            <div className='lock'></div>
                             <RoundButton 
-                            bgc={'#FFF'} 
-                            brc={'#343436'} 
-                            txtc={'#000000'} 
-                            txt={'Зарегистрироваться с Apple ID'} 
+                            bgc={'white'}
+                            brc={'rgba(54, 54, 54, 1)'}
+                            txtc={'black'}
+                            txt={'Зарегистрироваться с Apple ID'}
                             logo={appleLogo}
                             logoAlt={'Apple Logo'}
-                            isBold={true} />
+                            isBold={'true'}
+                            />
                         </div>
-                        <div className='part2'>
+                        <div className="rSection__or">
                             <p>или</p>
                         </div>
-                        <div className='part3'>
+                        <div className="rSection__regButton">
                             <RoundButton 
-                            bgc={'#1D9BF0'} 
-                            brc={'#343436'} 
-                            txtc={'#FFF'} 
-                            txt={'Зарегистрироваться'} 
-                            isBold={true} />
+                            bgc={'rgba(29, 155, 240, 1)'}
+                            brc={'rgba(29, 155, 240, 1)'}
+                            txtc={'white'}
+                            txt={'Зарегистрироваться'}
+                            logo={''}
+                            logoAlt={''}
+                            isBold={'true'}
+                            />
                         </div>
-                        <div className='part4'>
-                            <p>Регистрируясь, вы соглашаетесь с <span>Условиями предоставления услуг</span> и <span>Политикой конфиденциальности</span>, а также с <span>Политикой использования файлов cookie</span>.</p>
+                        <div className="rSection__rules">
+                            <p>Регистрируясь, вы соглашаетесь с <span>Условиями <br /> предоставления услуг</span> и <span>Политикой конфиденциальности</span>, а <br /> также с <span>Политикой использования файлов cookie</span>.</p>
                         </div>
-                        <div className="part5">
+                        <div className="rSection__loginButton">
                             <p>Уже зарегистрированы?</p>
                             <RoundButton 
-                            bgc={'#000000'} 
-                            brc={'#343436'} 
-                            txtc={'#1D9BF0'} 
-                            txt={'Войти'} 
-                            isBold={true} />
+                            bgc={'rgba(0, 0, 0, 0)'}
+                            brc={'rgba(54, 54, 54, 1)'}
+                            txtc={'rgba(29, 155, 240, 1)'}
+                            txt={'Войти'}
+                            logo={''}
+                            logoAlt={''}
+                            isBold={'true'}
+                            />
                         </div>
                     </div>
                 </div>
--- src/components/RoundButton/index.jsx
+++ src/components/RoundButton/index.jsx
@@ -8,7 +8,9 @@ export function RoundButton({bgc, brc, txtc, txt, logo, logoAlt, isBold = false}
             backgroundColor: `${bgc}`, 
             border: `1px solid ${brc}`, 
             color: `${txtc}`,
-            fontWeight: `${weightValue}`}}><img src={logo} alt={logoAlt} />{txt}</button>
+            fontWeight: `${weightValue}`}}
+        >
+            <img src={logo} alt={logoAlt} />{txt}</button>
         </>
     )
 }
